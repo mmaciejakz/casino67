7a5ba9066c6e023ae55ee7c43bcc44837c5c4aab
Nikodem <N.Kosowski@zset.leszno.pl>
2026-03-11 14:25:50 +0100

    a

--- connect.php
+++ connect.php
@@ -1,21 +1,21 @@
 <?php
-// connect.php - NAPRAWIONY
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_base = "casino67";
+// // connect.php - NAPRAWIONY
+// $db_server = "localhost";
+// $db_user = "root";
+// $db_pass = "";
+// $db_base = "casino67";
 
-// Tworzymy połączenie
-$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_base);
+// // Tworzymy połączenie
+// $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_base);
 
-// Sprawdzamy czy połączenie działa
-if (!$conn) {
-    die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
-}
+// // Sprawdzamy czy połączenie działa
+// if (!$conn) {
+//     die("Błąd połączenia z bazą danych: " . mysqli_connect_error());
+// }
 
-// Ustawiamy kodowanie
-mysqli_set_charset($conn, "utf8mb4");
+// // Ustawiamy kodowanie
+// mysqli_set_charset($conn, "utf8mb4");
 
-// Ustawiamy strefę czasową
-date_default_timezone_set('Europe/Warsaw');
+// // Ustawiamy strefę czasową
+// date_default_timezone_set('Europe/Warsaw');
 ?>
